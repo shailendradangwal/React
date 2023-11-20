162f4f0f186cb80deded3600c8f3aeb702d31372
Shailendra Dangwal <shailendra.dangwal@mrm.com>
2023-11-20 16:55:06 +0530

    Added extra cards using hoooks!!!

--- 09SwitchTheTheme/src/App.jsx
+++ 09SwitchTheTheme/src/App.jsx
@@ -24,16 +24,16 @@ function App() {
 
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-     <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+     <div className="w-full min-h-screen items-center">
+     
+        <div className="w-full flex justify-end mb-4">
           <ThemeBtn />
         </div>
 
-        <div className="w-full max-w-sm mx-auto">
+        <div className="w-full flex mx-auto">
           <Card />
         </div>                    
-      </div>        
+      
      </div>         
     </ThemeProvider>
   )
---
+++ 09SwitchTheTheme/src/components/childApi.js
@@ -0,0 +1,38 @@
+const desChild = [
+{
+    id:1,
+    image:"images/v1.png",
+    name : "Vritti Dangwal",
+    class : "Primary",
+    occassion : "Teachers' Day",
+    description : "She is a little ball of energy and curiosity. Every day is an adventure as she explores the world with wide-eyed wonder. Her laughter is infectious, and her boundless enthusiasm for the simplest things brings a special kind of joy to our home."
+},
+
+{
+id:2,
+image: "images/v2.png",
+name : "Vritti Dangwal",
+class : "Primary",
+occassion : "Diwali Celebration",
+description : "She is a reminder of the power of dreams, as she fearlessly reaches for the stars and embraces the challenges that come her way."
+},
+
+{
+id:3,
+image: "images/l1.png",
+name : "Lavitra Dangwal",
+class : "Fifth",
+occassion : "Bhai Dooj",
+description : "Blossoming into a unique individual with her own thoughts, dreams, and interests. She's developing a sense of independence, and her questions about the world showcase a growing intellect."
+},
+{
+id:4,
+image: "images/l2.png",
+name : "Lavitra Dangwal",
+class : "Fifth",
+occassion : "Diwali Celebration",
+description : "From school projects to hobbies, her talents are emerging, and you're witnessing the formation of her distinct personality. As she navigates the challenges of growing up, your role as a guiding presence becomes even more crucial, and the moments of connection and shared laughter are treasures that create a bond that transcends time."
+}
+]
+
+export default desChild;
